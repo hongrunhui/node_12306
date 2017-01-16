c8f1ab3af693485b7eab005d0d98282c57bc2a4c
MRHONG\hong <1037647264@qq.com>
2017-01-16 10:06:42 +0800

    增加学生票查询

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ fs.readFile('config.json','utf-8',function(err,data){
 		console.log('输入日期-time(如:2017-01-27)：');
 		config.time = scanf('%s');
 
-		console.log('输入车次-train_num(如:K1209，多个车次用|分开)：');
-		config.train_num = scanf('%s').split('|');
-
 		console.log('输入始发站-from_station(如:SNH)：');
 		config.from_station = scanf('%s');
 
 		console.log('输入终点站-end_station(如:SRG)：');
 		config.end_station = scanf('%s');
+
+		console.log('输入车次-train_num(如:K1209，多个车次用|分开)：');
+		config.train_num = scanf('%s').split('|');
 
 		console.log('输入邮箱-your_mail(如:123456789@163.com)：');
 		config.your_mail = scanf('%s');
@@ -32,6 +32,11 @@ fs.readFile('config.json','utf-8',function(err,data){
 		console.log('输入密码或者邮箱授权码-mail_pass：');
 		config.mail_pass = scanf('%s');
 
+		console.log('是否购买学生票?(y/n)：');
+		config.ticket_type = scanf('%s')=='y'?'0X00':'ADULT';
+
+		console.log('输入收件人邮箱(如果与上面的邮箱一致请直接回车)：');
+		config.receive_mail = scanf('%s');
 		// console.log(config);
 		fs.writeFile('config.json',JSON.stringify(config));
 	}else{
@@ -49,7 +54,7 @@ var yz_temp = [],yw_temp = [];//保存余票状态
 function queryTickets(config){
 	var options = { 
 	    hostname: 'kyfw.12306.cn',//12306
-	    path: '/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT',
+	    path: '/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes='+config.ticket_type,
 	    ca:[ca]//证书
 	};
 	var req = https.get(options, function(res){ 
@@ -69,6 +74,10 @@ function queryTickets(config){
     res.on('end',function(){
     	// console.log('res',data);
     	var jsonData = JSON.parse(data).data;
+    	if(!jsonData||jsonData.length == 0){
+    		console.log('没有查询到余票信息');
+    		return;
+    	}
     	var jsonMap = {};
     	for(var i=0;i<jsonData.length;i++){
     		var cur = jsonData[i];
@@ -94,7 +103,7 @@ function queryTickets(config){
 				}
 				var mailOptions = {
 				    from: config.your_mail, // 发件邮箱地址
-				    to: config.your_mail, // 收件邮箱地址，可以和发件邮箱一样
+				    to: config.receive_mail||config.your_mail, // 收件邮箱地址，可以和发件邮箱一样
 				    subject: trainNum+'有票啦，硬座：'+yz+'，硬卧：'+yw, // 邮件标题
 				    text: trainNum+'有票啦\n'+'时间是'+cur_train.start_train_date+',\n出发时间:'+cur_train.start_time+',\n到达时间:'+cur_train.arrive_time+',\n历时：'+cur_train.lishi+',\n始发站：'+cur_train.from_station_name+',\n到达：'+cur_train.to_station_name, // 邮件内容
 				};
