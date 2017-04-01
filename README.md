57f220fe5176a0c8cec265a0362f331b08a4567d
hongrunhui <15755191035@163.com>
2017-04-01 22:34:43 +0800

    update

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # node_12306
-##适用场景：
+## 适用场景：
 已经确定了火车的车次和时间（最好提前3-4天），但是没有座位了。想抢有座位的（只能抢别人的退票了，几天内一般都会有人退票）。
-##使用方法：
+## 使用方法：
   * 克隆代码到本地``` git clone https://github.com/hongrunhui/node_12306.git ``` 
   * 在当前文件夹终端``` npm install ```安装依赖
   * ```node main.js```，第一次运行会要求输入信息并且存入config.json，以后再次运行只会读取config.json中的数据，不会要求再次输入。如：<br/>
-  ![image](https://cloud.githubusercontent.com/assets/9162319/22016021/95f45e68-dcdf-11e6-8267-2d1b19532ce4.png)
+  ![image](https://cloud.githubusercontent.com/assets/9162319/24579567/816b61aa-172a-11e7-937b-84d7ff716a0e.png)
+  * 运行结果如下：<br/>
+  ![image](https://cloud.githubusercontent.com/assets/9162319/24579618/4979af30-172b-11e7-94b3-9feaa5053541.png)
   * ```node main.js -r```可以重写config.json(重新输入信息)。
   * 车次(```train_num```字段)可以输入多个车次，用|分开，如K123|K234(前提这些车次都在同一线路上)。
   
-##注意：
+## 注意：
   * 要使用邮件功能前提是要去邮箱设置里开启smtp;也可以用邮箱授权码代替，这种方式更安全。具体可以见[wiki](https://github.com/hongrunhui/node_12306/wiki/%E9%82%AE%E7%AE%B1%E8%AE%A4%E8%AF%81%E9%94%99%E8%AF%AF%E8%A7%A3%E5%86%B3%E5%8A%9E%E6%B3%95)
   * 针对有人提出程序可能会报错退出的问题，这里建议使用[pm2](http://pm2.keymetrics.io/)来启动```main.js```,具体步骤：
  	  * ```npm install pm2 -g```全局安装pm2
@@ -20,7 +22,9 @@
   * 有人提出自动抢票下单支付功能，这个我以后会想办法去实现。
   * [博客地址](http://www.cnblogs.com/hongrunhui/p/6284192.html)
 
-##更新：
+## 更新：
+  * 0.0.0.5
+    * 解决查询不成功问题
   * 0.0.0.4
     * 车站代码查询改为车站拼音查询
   * 0.0.0.3
@@ -36,6 +40,6 @@
   * 0.0.0.1
     * 实现基本功能
 
-##声明
+## 声明
   * 本程序最适合监听那些一张票都没有的车次(或者无座多，硬座/软座/硬卧/软卧无的车次)，通过本程序可以24小时监听车次是否会多出余票，并及时发送邮件通知个人(可以使用```pm2```部署在自己的服务器上或24小时开着电脑运行实现24小时监听)
   * 对于有特殊需求的可以自己修改代码或者咨询我。
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var ca = fs.readFileSync('./cert/srca.cer.pem');
 var nodemailer = require('nodemailer');
 var schedule = require('node-schedule');
 var scanf = require('scanf');
 var program = require('commander');
+var UA = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
 
 var config = {};
 program
@@ -14,7 +16,7 @@ program
 
 fs.readFile('config.json','utf-8',function(err,data){
 	if(err||!data||program.rewrite){
-		// console.log(err);
+
 		console.log('输入日期-time(如:2017-01-27)：');
 		config.time = scanf('%s');
 
@@ -57,11 +59,117 @@ fs.readFile('config.json','utf-8',function(err,data){
 	}
 	var rule = new schedule.RecurrenceRule();  
 	rule.second = [0];
+	queryTickets(config);
 	schedule.scheduleJob(rule, function(){
 		queryTickets(config);
-        console.log('scheduleCronstyle:' + new Date());
 	}); 
 });
+
+
+var yz_temp = [],yw_temp = [];//保存余票状态
+/*
+* 查询余票
+*/
+function queryTickets(config){
+	/*设置请求头参数*/
+	var options = { 
+	    hostname: 'kyfw.12306.cn',//12306
+	    port:443,
+	  	method:'GET',
+	    path: '/otn/leftTicket/queryX?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station.code+'&leftTicketDTO.to_station='+config.end_station.code+'&purpose_codes='+config.ticket_type,
+	    ca:[ca],//证书
+	    headers:{
+	      'Connection':'keep-alive',
+	      'Host':'kyfw.12306.cn',
+	      'User-Agent': UA,
+	      "Connection":"keep-alive",
+	      "Referer":"https://kyfw.12306.cn/otn/leftTicket/init",
+	      "Cookie":"__NRF=D2A7CA0EBB8DD82350AAB934FA35745B; JSESSIONID=0A02F03F9852081DDBFEA4AA03EF4252C569EB7AB1; _jc_save_detail=true; _jc_save_showIns=true; BIGipServerotn=1072693770.38945.0000; _jc_save_fromStation=%u77F3%u5BB6%u5E84%2CSJP; _jc_save_toStation=%u5408%u80A5%2CHFH; _jc_save_fromDate=2017-02-17; _jc_save_toDate=2017-01-19; _jc_save_wfdc_flag=dc",
+    	}
+	};
+	/*请求开始*/
+	var req = https.get(options, function(res){ 
+	    var data = '';
+	    /*设置邮箱信息*/
+	    var transporter = nodemailer.createTransport({
+		    host: "smtp.163.com",//邮箱的服务器地址，如果你要换其他类型邮箱（如QQ）的话，你要去找他们对应的服务器，
+		    secureConnection: true,
+		    port:465,//端口，这些都是163给定的，自己到网上查163邮箱的服务器信息
+		    auth: {
+		        user: config.your_mail,//邮箱账号
+		        pass: config.mail_pass,//邮箱密码
+		    }
+		});
+	    res.on('data',function(buff){
+	    	data += buff;//查询结果（JSON格式）
+	    }); 
+	    res.on('end',function(){
+	    	var jsonData;//用来保存返回的json数据
+	    	try{
+	    	 	jsonData = JSON.parse(data).data;
+	    	}catch(e){
+	    		console.log('JSON数据出错',e);
+	    		return;
+	    	}
+	    	if(!jsonData||jsonData.length == 0){
+	    		console.log('没有查询到余票信息');
+	    		return;
+	    	}
+	    	/*获取车次与车辆代码的映射表*/
+	    	var jsonMap = {};
+	    	for(var i=0;i<jsonData.length;i++){
+	    		var cur = jsonData[i];
+	    		jsonMap[cur.queryLeftNewDTO.station_train_code] = cur.queryLeftNewDTO;
+	    		
+	    	}
+	    	/*过滤不需要的车次*/
+	    	var train_arr = config.train_num;
+	    	for(var j = 0;j < train_arr.length;j++){
+				var cur_train = jsonMap[train_arr[j]];//当前车次
+				if(!cur_train){
+					console.log('当天没有'+train_arr[j]+'这趟车次');
+					continue;
+				}
+				var yz = cur_train.yz_num;//硬座数目
+				var yw = cur_train.yw_num;//硬卧数目
+				var trainNum = cur_train.station_train_code;//车次
+				console.log('\n '+trainNum+' 车次的硬座余票数:',yz,', 硬卧余票数:',yw,'。当前时间：'+getTime());
+				if(yz!='无'&&yz!='--'||yw!='无'&&yw!='--'){
+					if(yw_temp[j] == yw && yz_temp[j] == yz){//当余票状态发生改变的时候就不发送邮件
+						console.log(' '+trainNum+'车次状态没改变，不重复发邮件');
+						continue;
+					}
+					var mailOptions = {
+					    from: config.your_mail, // 发件邮箱地址
+					    to: config.receive_mail||config.your_mail, // 收件邮箱地址，可以和发件邮箱一样
+					    subject: trainNum+'有票啦，硬座：'+yz+'，硬卧：'+yw, // 邮件标题
+					    text: trainNum+'有票啦\n'+cur_train.from_station_name+'=============>'+cur_train.to_station_name+'\n出发日期：'+config.time+',\n出发时间：'+cur_train.start_time+',\n到达时间：'+cur_train.arrive_time+',\n历时：'+cur_train.lishi, // 邮件内容
+					};
+
+					// 发邮件部分
+					(function(j,yz,yw){
+						transporter.sendMail(mailOptions, function(error, info){
+						    if(error){
+						        return console.log(error);
+						    }
+						    console.log(' 邮件已发送: ====================> ' + mailOptions.to);
+						    yw_temp[j] = yw;//保存当前列车的余票数量
+						    yz_temp[j] = yz;
+						});
+					})(j,yz,yw);
+				}else{
+					console.log(trainNum+'硬座/硬卧无票');
+				}
+	    	}
+	    	// fs.writeFile('./train.json',data);
+	    });  
+	});
+
+	req.on('error', function(err){
+	    console.error(err.code);
+	});
+}
+
 /*
 * 爬取全国车站信息并生成JSON文件
 */
@@ -108,98 +216,9 @@ function stationJson(){
     	console.error(err.code);
 	});
 }
-
-var yz_temp = [],yw_temp = [];//保存余票状态
-/*
-* 查询余票
-*/
-function queryTickets(config){
-
-	var options = { 
-	    hostname: 'kyfw.12306.cn',//12306
-	    path: '/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station.code+'&leftTicketDTO.to_station='+config.end_station.code+'&purpose_codes='+config.ticket_type,
-	    ca:[ca]//证书
-	};
-	var req = https.get(options, function(res){ 
-    var data = '';
-    var transporter = nodemailer.createTransport({
-	    host: "smtp.163.com",//邮箱的服务器地址，如果你要换其他类型邮箱（如QQ）的话，你要去找他们对应的服务器，
-	    secureConnection: true,
-	    port:465,//端口，这些都是163给定的，自己到网上查163邮箱的服务器信息
-	    auth: {
-	        user: config.your_mail,//邮箱账号
-	        pass: config.mail_pass,//邮箱密码
-	    }
-	});
-    res.on('data',function(buff){
-    	data += buff;//查询结果（JSON格式）
-    }); 
-    res.on('end',function(){
-    	// console.log('res',data);
-    	var jsonData;
-    	try{
-    	 	jsonData = JSON.parse(data).data;
-    	}catch(e){
-    		console.log('JSON数据出错',e);
-    		return;
-    	}
-    	if(!jsonData||jsonData.length == 0){
-    		console.log('没有查询到余票信息');
-    		return;
-    	}
-    	var jsonMap = {};
-    	for(var i=0;i<jsonData.length;i++){
-    		var cur = jsonData[i];
-    		jsonMap[cur.queryLeftNewDTO.station_train_code] = cur.queryLeftNewDTO;
-    		
-    	}
-    	var train_arr = config.train_num;
-    	for(var j = 0;j < train_arr.length;j++){
-			var cur_train = jsonMap[train_arr[j]];
-			if(!cur_train){
-				console.log('当天没有'+train_arr[j]+'这趟车次');
-				continue;
-			}
-			var yz = cur_train.yz_num;//硬座数目
-			var yw = cur_train.yw_num;//硬卧数目
-			var trainNum = cur_train.station_train_code;//车次
-			console.log(trainNum+'硬座',yz);
-			console.log(trainNum+'硬卧',yw);
-			if(yz!='无'&&yz!='--'||yw!='无'&&yw!='--'){
-				if(yw_temp[j] == yw && yz_temp[j] == yz){//当余票状态发生改变的时候就不发送邮件
-					console.log(trainNum+'状态没改变，不重复发邮件');
-					continue;
-				}
-				var mailOptions = {
-				    from: config.your_mail, // 发件邮箱地址
-				    to: config.receive_mail||config.your_mail, // 收件邮箱地址，可以和发件邮箱一样
-				    subject: trainNum+'有票啦，硬座：'+yz+'，硬卧：'+yw, // 邮件标题
-				    text: trainNum+'有票啦\n'+'时间是'+cur_train.start_train_date+',\n出发时间:'+cur_train.start_time+',\n到达时间:'+cur_train.arrive_time+',\n历时：'+cur_train.lishi+',\n始发站：'+cur_train.from_station_name+',\n到达：'+cur_train.to_station_name, // 邮件内容
-				};
-
-				// 发邮件部分
-				(function(j,yz,yw){
-					console.log(j,yw);
-					transporter.sendMail(mailOptions, function(error, info){
-					    if(error){
-					        return console.log(error);
-					    }
-					    console.log('邮件已发送: ' + info.response);
-					    yw_temp[j] = yw;//保存当前列车的余票数量
-					    yz_temp[j] = yz;
-					});
-				})(j,yz,yw);
-			}else{
-				console.log(trainNum+'硬座/硬卧无票');
-			}
-    	}
-    	// fs.writeFile('./train.json',data);
-    })  
-});
-
-req.on('error', function(err){
-    console.error(err.code);
-});
+function getTime(){
+	var T = new Date();
+	return T.getFullYear()+'-'+(parseInt(T.getMonth())+1)+'-'+T.getDate()+' '+T.getHours()+":"+T.getMinutes()+":"+T.getSeconds();
 }
 
 
