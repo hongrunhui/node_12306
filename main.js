0a9b84fb7ca7e0116a4df193233ab090cd73faaa
lhlybly <15755191035@163.com>
2018-01-03 20:51:17 +0800

    update

--- main.js
+++ main.js
@@ -9,54 +9,127 @@ const program = require('commander');
 const UA = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
 const inquirer = require('inquirer');
 var config = {};
-console.log(process.argv);
-return;
-program
-	.version('0.0.1')
-	.option('-r, --rewrite', 'rewrite config')
-	.parse(process.argv);
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+var prompt = inquirer.createPromptModule();
+let _stations = JSON.parse(fs.readFileSync('station.json', 'utf-8'));
+let isRewrite = hasArgv(process.argv, '-r');
+function hasArgv(argv, filter) {
+	argv = argv.slice(2);
+	return argv.some((item, i) => {
+		return filter;
+	});
+}
+function searchTrain(answers, input) {
+	input = input || '';
+	console.log(input);
+	return;
+	if (Object.prototype.toString.call(_stations.stationInfo[input]) === '[object Array]') {
 
+	}
+	else {
+
+	}
+}
+let questions = [
+	{
+		type: 'input',
+		name: 'time',
+		message: '输入日期-time(如:2017-01-27)：',
+		validate(input) {
+			let re = /[\d]{4}-[\d]{1,2}-[\d]{1,2}/ig;
+			if (input.match(re)) {
+				return true;
+			}
+			else {
+				console.log(' (输入的日期非法，重新输入)');
+				return false;
+			}
+		}
+	},
+	{
+		type: 'autocomplete',
+		name: 'from_station',
+		message: '输入始发站拼音-from_station(如:shanghai)：',
+		source: searchTrain,
+		// validate(input) {
+		// 	if (_stations.stationInfo[input]) {
+		// 		return true;
+		// 	}
+		// 	else {
+		// 		console.log(' (没有这个车站哦，请重新输入)');
+		// 		return false;
+		// 	}
+		// }
+	},
+	{
+		type: 'input',
+		name: 'end_station',
+		message: '输入终点站拼音-end_station(如:hefei)：',
+		validate(input) {
+			if (_stations.stationInfo[input]) {
+				return true;
+			}
+			else {
+				console.log(' (没有这个车站哦，请重新输入)');
+				return false;
+			}
+		}
+	},
+	{
+		type: 'input',
+		name: 'train_num',
+		message: '输入车次-train_num(如:K1209，多个车次用|分开)：',
+		validate(input) {
+			return true;
+		}
+	},
+	{
+		type: 'input',
+		name: 'your_mail',
+		message: '输入邮箱-your_mail(如:123456789@163.com)：',
+		validate(input) {
+			return true;
+		}
+	},
+	{
+		type: 'password',
+		name: 'mail_pass',
+		message: '输入密码或者邮箱授权码-mail_pass：',
+		validate(input) {
+			return true;
+		}
+	},
+	{
+		type: 'confirm',
+		name: 'ticket_type',
+		message: '是否购买学生票?(y/n)：',
+		validate(input) {
+			return true;
+		}
+	},
+	{
+		type: 'input',
+		name: 'receive_mail',
+		message: '输入收件人邮箱(如果与上面的邮箱一致请直接回车)：',
+		validate(input) {
+			return true;
+		}
+	}
+];
 fs.readFile('config.json', 'utf-8', function (err, data) {
-	if (err || !data || program.rewrite) {
-
-		console.log('输入日期-time(如:2017-01-27)：');
-		config.time = scanf('%s');
-
-		var _stations = JSON.parse(fs.readFileSync('station.json', 'utf-8'));
-
-		console.log('输入始发站拼音-from_station(如:shanghai)：');
-		var _start = scanf("%s");
-		while (!_stations.stationInfo[_start]) {
-			console.log('没有这个车站哦，请重新输入：');
-			_start = scanf("%s");
-		}
-		config.from_station = _stations.stationInfo[_start];
-		console.log('输入终点站拼音-end_station(如:hefei)：');
-		var _end = scanf('%s');
-		while (!_stations.stationInfo[_end]) {
-			console.log('没有这个车站哦，请重新输入：');
-			_end = scanf("%s");
-		}
-		config.end_station = _stations.stationInfo[_end];
-
-		console.log('输入车次-train_num(如:K1209，多个车次用|分开)：');
-		config.train_num = scanf('%s').split('|');
-
-		console.log('输入邮箱-your_mail(如:123456789@163.com)：');
-		config.your_mail = scanf('%s');
-
-		console.log('输入密码或者邮箱授权码-mail_pass：');
-		config.mail_pass = scanf('%s');
-
-		console.log('是否购买学生票?(y/n)：');
-		config.ticket_type = scanf('%s') == 'y' ? '0X00' : 'ADULT';
-
-		console.log('输入收件人邮箱(如果与上面的邮箱一致请直接回车)：');
-		config.receive_mail = scanf('%s');
-		// console.log(config);
-		fs.writeFile('config.json', JSON.stringify(config));
-	} else {
-		// console.log(data);
+	if (err || !data || isRewrite) {
+		prompt(questions).then(answer => {
+			answer.from_station = _stations.stationInfo[answer.from_station];	
+			answer.end_station = _stations.stationInfo[answer.end_station];	
+			answer.train_num = answer.train_num.split('|');
+			answer.ticket_type = answer.ticket_type ? '0x00' : 'ADULT';
+			answer.receive_mail = answer.receive_mail || answer.your_mail;
+			config = answer;
+			console.log(config);
+			fs.writeFile('config.json', JSON.stringify(config));	
+		});
+	}
+	else {
 		config = JSON.parse(data);
 	}
 	var rule = new schedule.RecurrenceRule();
@@ -67,12 +140,69 @@ fs.readFile('config.json', 'utf-8', function (err, data) {
 	});
 });
 
+// program
+// 	.version('0.0.1')
+// 	.option('-r, --rewrite', 'rewrite config')
+// 	.parse(process.argv);
+
+// fs.readFile('config.json', 'utf-8', function (err, data) {
+// 	if (err || !data || program.rewrite) {
+
+// 		console.log('输入日期-time(如:2017-01-27)：');
+// 		config.time = scanf('%s');
+
+// 		var _stations = JSON.parse(fs.readFileSync('station.json', 'utf-8'));
+
+// 		console.log('输入始发站拼音-from_station(如:shanghai)：');
+// 		var _start = scanf("%s");
+// 		while (!_stations.stationInfo[_start]) {
+// 			console.log('没有这个车站哦，请重新输入：');
+// 			_start = scanf("%s");
+// 		}
+// 		config.from_station = _stations.stationInfo[_start];
+// 		console.log('输入终点站拼音-end_station(如:hefei)：');
+// 		var _end = scanf('%s');
+// 		while (!_stations.stationInfo[_end]) {
+// 			console.log('没有这个车站哦，请重新输入：');
+// 			_end = scanf("%s");
+// 		}
+// 		config.end_station = _stations.stationInfo[_end];
+
+// 		console.log('输入车次-train_num(如:K1209，多个车次用|分开)：');
+// 		config.train_num = scanf('%s').split('|');
+
+// 		console.log('输入邮箱-your_mail(如:123456789@163.com)：');
+// 		config.your_mail = scanf('%s');
+
+// 		console.log('输入密码或者邮箱授权码-mail_pass：');
+// 		config.mail_pass = scanf('%s');
+
+// 		console.log('是否购买学生票?(y/n)：');
+// 		config.ticket_type = scanf('%s') == 'y' ? '0X00' : 'ADULT';
+
+// 		console.log('输入收件人邮箱(如果与上面的邮箱一致请直接回车)：');
+// 		config.receive_mail = scanf('%s');
+// 		// console.log(config);
+// 		fs.writeFile('config.json', JSON.stringify(config));
+// 	} else {
+// 		// console.log(data);
+// 		config = JSON.parse(data);
+// 	}
+// 	var rule = new schedule.RecurrenceRule();
+// 	rule.second = [0];
+// 	queryTickets(config);
+// 	schedule.scheduleJob(rule, function () {
+// 		queryTickets(config);
+// 	});
+// });
+
 
 var yz_temp = [], yw_temp = [];//保存余票状态
 /*
 * 查询余票
 */
 function queryTickets(config) {
+	console.log(config);
 	/*设置请求头参数*/
 	var options = {
 		hostname: 'kyfw.12306.cn',//12306
@@ -162,12 +292,13 @@ function queryTickets(config) {
 			var trainData;
 			//用来保存返回的json数据
 			var trainMap;
+			console.log(data);
 			try {
 				var _data = JSON.parse(data).data;
-				trainData = _data.result;
-				trainMap = _data.map;
+				trainData = _data && _data.result;
+				trainMap = _data && _data.map;
 			} catch (e) {
-				console.log('JSON数据出错', e);
+				console.log('JSON数据出错,请检查输入配置是否正确', e);
 				return;
 			}
 			jsonData = b4(trainData, trainMap);
